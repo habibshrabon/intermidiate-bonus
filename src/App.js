8f70770142a2c35bdeb06144268fe8776f6469d9
habibshrabon <snhabib777@gmail.com>
2021-03-14 23:13:40 +0600

    added

---
+++ src/App.js
@@ -0,0 +1,50 @@
+
+import './App.css';
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [likeColor, setLikeColor] = useState('');
+  const [users, setUsers] = useState([]);
+  const [singelUser, setSingleUser] = useState({});
+  const [randomUser, setRandomUser] = useState({});
+
+  useEffect(() => {
+    const url = `https://jsonplaceholder.typicode.com/users`
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setUsers(data))
+
+      //single user
+    fetch('https://jsonplaceholder.typicode.com/users/1')
+    .then(res => res.json())
+    .then(data => setSingleUser(data))
+
+    //random user
+    fetch('https://randomuser.me/api/')
+    .then(res => res.json())
+    .then(data => setRandomUser(data.results[0]))
+  }, [])
+
+  const handelLike = () => {
+    const color = likeColor ? '' : 'primary';
+    setLikeColor(color);
+  }
+  return (
+    <div>
+      <AccessAlarmIcon></AccessAlarmIcon>
+      <ThumbUpAltIcon onClick={handelLike} color={likeColor}></ThumbUpAltIcon>
+      <h1>Name: {singelUser.name}</h1>
+      <h2>Random Gender: {randomUser.name?.first}</h2>
+
+      {
+        users.map(user => <li>{user.name}</li>)
+      }
+      
+     
+    </div>
+  );
+}
+
+export default App;
